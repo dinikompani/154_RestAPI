3da690c7c94ecde40acb42834f318eaf9cd77bd9
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2023-12-28 21:40:23 +0700

    add IconButton pada fun KontakCard

--- app/src/main/java/com/example/contact/ui/home/screen/HomeScreen.kt
+++ app/src/main/java/com/example/contact/ui/home/screen/HomeScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -115,10 +116,13 @@ fun KontakCard(
                     style = MaterialTheme.typography.titleLarge,
                 )
                 Spacer(modifier.weight(1f))
-                Icon(
-                    imageVector = Icons.Default.Phone,
-                    contentDescription = null,
-                )
+                IconButton(onClick = { onDeleteClick(kontak) }) {
+                    Icon(
+                        imageVector = Icons.Default.Phone,
+                        contentDescription = null,
+                    )
+                }
+
                 Text(
                     text = kontak.nohp,
                     style = MaterialTheme.typography.titleMedium
