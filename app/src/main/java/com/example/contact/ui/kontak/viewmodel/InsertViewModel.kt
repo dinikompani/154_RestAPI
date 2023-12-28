7c8e657608b36818869b6a31fb966184bd0d3bc4
TA1154 <wahdini.a.ft21@mail.umy.ac.id>
2023-12-28 22:42:15 +0700

    add InsertViewModel

---
+++ app/src/main/java/com/example/contact/ui/kontak/viewmodel/InsertViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.contact.ui.kontak.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.contact.repository.KontakRepository
+
+class InsertViewModel (private val kontakRepository: KontakRepository) : ViewModel() {
+
+    var insertKontakState by mutableStateOf(InsertUiState())
+        private set
+}
